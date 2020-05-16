5c71f4d40294a5f02d05aa6fd3a57119b7d920eb
John Coates <john@johncoates.dev>
2020-05-15 21:18:21 -0500

    Change Library to Framework

---
+++ Resources/RogueFramework.h
@@ -0,0 +1,14 @@
+//
+//  RogueFramework.h
+//  Created on 5/15/20
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RogueFramework.
+FOUNDATION_EXPORT double RogueFrameworkVersionNumber;
+
+//! Project version string for RogueFramework.
+FOUNDATION_EXPORT const unsigned char RogueFrameworkVersionString[];
+
+#import <RogueFramework/RogueHook.h>
