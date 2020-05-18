f37ab0f1ecccca149d267683ad1020192a40a0cf
John Coates <john@johncoates.dev>
2020-05-18 15:16:49 -0500

    Switch back to .framework

--- Scripts/buildLibrary.sh
+++ Scripts/buildLibrary.sh
@@ -5,12 +5,13 @@ scriptsDirectory=`dirname "$0"`
 projectDirectory="$scriptsDirectory/../"
 
 pushd "$projectDirectory"
-    xcodebuild archive -project ./RogueFramework.xcodeproj -scheme Rogue -configuration Release -destination "generic/platform=iOS" \
-    -archivePath "./build/ios.xcarchive"
-    xcodebuild archive -project ./RogueFramework.xcodeproj -scheme Rogue -configuration Release -destination "generic/platform=iOS Simulator" \
-    -archivePath "./build/ios-simulator.xcarchive"
-    
-    subpath="/Products/Library/Frameworks/Rogue.framework"
-    xcodebuild -create-xcframework -framework "./build/ios.xcarchive/$subpath" -framework "./build/ios-simulator.xcarchive/$subpath" \
-    -output ./build/Rogue.xcframework
+    xcodebuild -scheme Rogue -project RogueFramework.xcodeproj -sdk iphoneos -configuration Release -arch arm64e DISTRIBUTING=YES WRAPPER_EXTENSION="temp"
+    xcodebuild -scheme Rogue -project RogueFramework.xcodeproj -sdk iphoneos -configuration Release -arch arm64 DISTRIBUTING=YES WRAPPER_EXTENSION="temp"
+    xcodebuild -scheme Rogue -project RogueFramework.xcodeproj -sdk iphoneos -configuration Release -arch armv7 DISTRIBUTING=YES WRAPPER_EXTENSION="temp"
+    xcodebuild -scheme Rogue -project RogueFramework.xcodeproj -sdk iphonesimulator -configuration Release -arch x86_64 DISTRIBUTING=YES WRAPPER_EXTENSION="temp"
+popd
+
+pushd "$projectDirectory/build"
+    libtool -static Rogue.arm64e.a Rogue.arm64.a Rogue.armv7.a Rogue.x86_64.a -o Rogue.temp/Rogue
+    mv Rogue.temp Rogue.framework
 popd
